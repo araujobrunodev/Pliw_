658d1620fcecace6de213e0b1a3d9fc534c13958
unknown <ballogggamer@gmail.com>
2024-07-05 15:17:08 -0300

    add ChangePages

---
+++ Pliw/Assets/scripts/ChangePage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePage : MonoBehaviour
+{
+    public Image HomePage;
+    public Image PlayPage;
+    private static bool homePageActive = false;
+    private static bool playPageActive = false;
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        homePageActive = true;
+        playPageActive = false;
+        button.onClick.AddListener(Change);
+        receive();
+    }
+
+    void receive () {
+        HomePage.gameObject.SetActive(homePageActive);
+        PlayPage.gameObject.SetActive(playPageActive);
+    }
+
+    void Change () {
+        if (homePageActive) {
+            homePageActive = false;
+            playPageActive = true;
+        } else {
+            homePageActive = true;
+            playPageActive = false;
+        }
+
+        receive();
+    }
+}
