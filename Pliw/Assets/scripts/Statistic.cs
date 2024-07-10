dbd449fa48f3967874d14c123c4b5ce981094c52
unknown <ballogggamer@gmail.com>
2024-07-10 20:36:44 -0300

    add save to Statistic

--- Pliw/Assets/scripts/Statistic.cs
+++ Pliw/Assets/scripts/Statistic.cs
@@ -6,15 +6,38 @@ using TMPro;
 
 public class Statistic : MonoBehaviour
 {
-    public TMP_Text scoreText, bestScoreText, AllClicksText, AllTheDeathsText;
-    public static int score, bestScore, AllClicks, AllTheDeaths;
-
+    public TMP_Text scoreText, bestScoreText, AllClicksText, AllLostText;
+    public static int score, bestScore, AllClicks, allLost;
+    private static string path = "./Assets/load/savedVariables.json";
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Current score: " + score;
         bestScoreText.text = "best score: " + bestScore;
         AllClicksText.text = "All clicks: " + AllClicks;
-        AllTheDeathsText.text = "All the deaths: " + AllTheDeaths;
+        AllLostText.text = "All lost: " + allLost;
     }
+
+    public static void save () {
+        var obj = new Statistic_variables();
+        if (bestScore < score) bestScore = score;
+        allLost++;
+        
+        obj.score = score;
+        obj.bestscore = bestScore;
+        obj.allLost = allLost;
+        obj.allClicks = AllClicks;
+
+        var objJSON = JsonUtility.ToJson(obj);
+        
+        System.IO.File.WriteAllText(path, objJSON);
+    }
+}
+
+
+class Statistic_variables {
+    public int score; 
+    public int bestscore;
+    public int allClicks;
+    public int allLost;
 }
